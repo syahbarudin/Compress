b314d678882c63268f2bdd2143420eb824589cdc
Syahbarudin Abdillah <146621192+syahbarudin@users.noreply.github.com>
2026-09-26 15:32:45 +0700

    Update press.py

--- press.py
+++ press.py
@@ -13,7 +13,7 @@ from streamlit_sortables import sort_items
 # --- KONFIGURASI HALAMAN ---
 st.set_page_config(
     page_title="File Compress",
-    page_icon="face.png" if os.path.exists("face.png") else "🗜️",
+    page_icon="star.png" if os.path.exists("face.png") else "🗜️",
     layout="wide",
     initial_sidebar_state="expanded",
 )
